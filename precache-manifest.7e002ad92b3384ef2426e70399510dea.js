6b178930f2fa00a39b4d0908c749947b9c10d0ac
Vilmantas Cepokas <vilmantasc@wix.com>
2020-03-29 19:13:53 +0300

    Updates

---
+++ precache-manifest.7e002ad92b3384ef2426e70399510dea.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3ec3591e2323f20d71f6dc8ce239e9c",
+    "url": "/kulkaa.github.io/servicesite/index.html"
+  },
+  {
+    "revision": "426b76c615037f349f28",
+    "url": "/kulkaa.github.io/servicesite/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8fac0c81aa311a5ee996",
+    "url": "/kulkaa.github.io/servicesite/static/js/2.ccb8f1d2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/kulkaa.github.io/servicesite/static/js/2.ccb8f1d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "426b76c615037f349f28",
+    "url": "/kulkaa.github.io/servicesite/static/js/main.cd0608f3.chunk.js"
+  },
+  {
+    "revision": "b7ea1fd4351802e1d1b0",
+    "url": "/kulkaa.github.io/servicesite/static/js/runtime-main.3558344c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/kulkaa.github.io/servicesite/static/media/logo.5d5d9eef.svg"
+  }
+]);
