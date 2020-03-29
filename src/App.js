ac00c0f3a1d50dda3cea21de581868eea2a1ca4a
Vilmantas Cepokas <vilmantasc@wix.com>
2020-03-29 19:34:11 +0300

    app change

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React from "react";
+import logo from "./logo.svg";
 import "./App.css";
+import * as barbora from "./barbora";
 
 function App() {
   return (
     <div className="App">
-      <h1>Test</h1>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <button onClick={barbora.main}>Click me!</button>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
