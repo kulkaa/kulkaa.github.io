a8a388d993d80cd36e8bfd54e3a2a6aa07118009
Vilmantas Cepokas <vilmantasc@wix.com>
2020-03-29 19:27:02 +0300

    app changes

--- src/App.js
+++ src/App.js
@@ -6,7 +6,21 @@ import * as barbora from "./barbora";
 function App() {
   return (
     <div className="App">
-      <h1>Test</h1>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <button onClick={barbora.main}>Click me!</button>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
