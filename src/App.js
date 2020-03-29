fb8177d2440cf4ad611fe6357f06656499040f70
Vilmantas Cepokas <vilmantasc@wix.com>
2020-03-29 19:39:23 +0300

    default app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import * as barbora from "./barbora";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <button onClick={barbora.main}>Click me!</button>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
